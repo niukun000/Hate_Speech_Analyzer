cdd3b27246deffb02e806630d8fc2668575f94c9
Kun Niu <kun000.niu@gmail.com>
2020-01-21 22:38:49 -0600

    Delete logistic_regression.py

--- logistic_regression.py
+++
@@ -1,6 +0,0 @@
-from traditional_methods import get_data, traditional
-from sklearn.linear_model import LogisticRegression
-
-data = get_data.read_file("labeled_data.csv")
-logistic_regression_method = traditional.Traditional_method(LogisticRegression(max_iter=1000), "logistic regression")
-logistic_regression_method.classification_report(get_data.upsampling(data))
