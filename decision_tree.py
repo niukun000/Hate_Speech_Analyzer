50f492e476e3e3e62761c75e22b9084b1c408fdd
Kun Niu <kun000.niu@gmail.com>
2020-01-21 22:37:58 -0600

    Delete decision_tree.py

--- decision_tree.py
+++
@@ -1,6 +0,0 @@
-from sklearn.tree import DecisionTreeClassifier
-from traditional_methods import get_data, traditional
-
-data = get_data.read_file("labeled_data.csv")
-decision_tree_method = traditional.Traditional_method( DecisionTreeClassifier(), "DecisionTreeClassifier")
-decision_tree_method.classification_report(get_data.upsampling(data), "upsampling")
