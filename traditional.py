c6d7eb5cdeb8a01a48d8fa4897ff5561d3ae5554
Kun Niu <kun000.niu@gmail.com>
2020-01-30 23:46:04 -0600

    upsampling before split
    it did not work so well

--- traditional.py
+++ traditional.py
@@ -1,12 +1,21 @@
 import numpy as np
+from scipy.sparse import issparse, isspmatrix
 from sklearn import pipeline, feature_extraction, svm, metrics
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
 from sklearn.model_selection import KFold
+from sklearn.svm import LinearSVC
+
+
 from traditional_methods import get_data
 import time
 import numpy as np
+from imblearn.over_sampling import SMOTE, RandomOverSampler
+# from sklearn.metrics import F
+
 class Traditional_method:
     def __init__(self, model, model_name="unknown model"):
+        # super()
+        super().__init__()
         '''
         Build tradition method class
         :param file: csv file
@@ -14,26 +23,20 @@ class Traditional_method:
         :param model: sckit learn model
         '''
         self.model = pipeline.Pipeline([
-            ('counts', feature_extraction.text.CountVectorizer(min_df=1)),
+
+            ('counts', feature_extraction.text.CountVectorizer(
+                min_df=5, stop_words="english",analyzer="word", ngram_range=(1,2)
+            )
+             ),
             ('tfidf', feature_extraction.text.TfidfTransformer()),
             (model_name, model),
         ])
         # self.data = data
         self.model_name = model_name
 
-    def classification_report(self, data, data_type="unbalanced"):
-        # data= self.data
+    def k_fold(self, data_type):
         print(data_type + " data")
-        # X_train, X_test, y_train, y_test = train_test_split(data['tweet'], data['class'], random_state=0)
-        # start_time = time.time()
-        # self.model.fit(X_train, y_train)
-        # fit_time = time.time()
-        #
-        # y_pred = self.model.predict(X_test)
-        #
-        #
-        # print('Accuracy of '+ self.model_name + '= {}'.format(
-        #     np.mean(y_pred == y_test)))
+
         kfold = KFold(5, True, 1)
         # enumerate splits
         X = np.array(data["tweet"])
@@ -51,41 +54,72 @@ class Traditional_method:
 
             print('Accuracy of ' + self.model_name + '= {}'.format(
                 np.mean(y_pred == y_test)))
+            print(metrics.classification_report(
+                y_test, y_pred, target_names=["hate_speech", "offensive_language", "neither"]))
+    # def upsampling(self, data):
+    def classification_report(self, X, y , data_type="unbalanced"):
+        # data= self.data
+        # X, y = data["tweet"], data["class"]
+        clf = self.model
+        scores = cross_val_score(clf, X, y, cv=5)
+        # print("cross validation score", scores)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+        # sm = SMOTE()
+        # X_train, X_test = sm.fit_sample(X_train, y_train.ravel())
+        X_train, y_train = get_data.up_sampling(X_train, y_train)
+        start_time = time.time()
+        self.model.fit(X_train, y_train)
+        fit_time = time.time()
 
+        y_pred = self.model.predict(X_test)
 
+        print(self.model_name + " with " + data_type)
+        print(metrics.classification_report(
+                y_test, y_pred, target_names=["hate_speech", "offensive_language", "neither"]))
+
+        print("time comsume to fit model", fit_time - start_time)
+        print()
+        print('Accuracy of '+ self.model_name + 'on test sets= {}'.format(
+            np.mean(y_pred == y_test)))
+        print()
+        X_pred = self.model.predict(X_train)
+        print("Accuracy on training set", np.mean(y_train == X_pred))
+        print(0)
+        print("cross validation score", scores)
+        print()
+
+    def classify(self, X, y):
+        # self.split(X,y)
+        cv = StratifiedKFold(n_splits=5)
+        for train_idx, test_idx, in cv.split(X, y):
+            X_train, y_train = X[train_idx], y[train_idx]
+            X_test, y_test = X[test_idx], y[test_idx]
+            # print(np.array(X_train))
+            # X_train, y_train = RandomOverSampler().fit_sample(X_train.as_metrix(), y_train.ravel())
+            # X_resampled, y_resampled = ros.fit_resample(X, y)
+
+            X_train, y_train = get_data.up_sampling(X_train, y_train)
+            self.model.fit(X_train, y_train)
+            # fit_time = time.time()
+
+            y_pred = self.model.predict(X_test)
             print(metrics.classification_report(
                 y_test, y_pred, target_names=["hate_speech", "offensive_language", "neither"]))
 
-        # print("time comsume to fit model", fit_time - start_time)
-        # print("time consume to predict results", time.time() - fit_time)
-        # print("upsampling")
-        #
-        # X_train, X_test, y_train, y_test = train_test_split(data1['tweet'], data1['class'], random_state=0)
-        # self.model.fit(X_train, y_train)
-        # y_pred = self.model.predict(X_test)
-        #
-        # print('Accuracy of '+ self.model_name + '= {}'.format(
-        #     np.mean(y_pred == y_test)))
-        #
-        # print(metrics.classification_report(
-        #     y_test, y_pred, target_names=["hate_speech", "offensive_language", "neither"]))
-        #
-        # print("downsampling")
-        # X_train, X_test, y_train, y_test = train_test_split(data2['tweet'], data2['class'], random_state=0)
-        # self.model.fit(X_train, y_train)
-        # y_pred = self.model.predict(X_test)
-        #
-        # # print('Accuracy of SVM= {}'.format(
-        # #     np.mean(y_pred == y_test)))
-        # print('Accuracy of ' + self.model_name + '= {}'.format(
-        #     np.mean(y_pred == y_test)))
-        # print(metrics.classification_report(
-        #     y_test, y_pred, target_names=["hate_speech", "offensive_language", "neither"]))
+
 if __name__ == '__main__':
     # data = clean_text(df)
     # data1 = upsampling(clean_text((df)))
     # data2 = downsampling(clean_text(df))
-    data = get_data.read_file("labeled_data.csv")
-    svm = Traditional_method( svm.LinearSVC(), "LinearSVC")
-    svm.classification_report(data)
+    X, y = get_data.read_file("labeled_data.csv")
+    # x_T = []
+    # print(list(X))
+    # for i in X:
+    #     print(i)
+    # X, y = SMOTE().fit_sample(X, y)
 
+    # svm = Traditional_method( svm.LinearSVC(), "LinearSVC")
+    # svm.classification_report(get_data.upsampling(X, y), "upsampling data")
+
+    linear_svc = Traditional_method(LinearSVC(), "LinearSVC")
+    linear_svc.classify(X, y)
